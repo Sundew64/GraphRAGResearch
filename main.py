889dce7c38da2e6ee66707db03810306e2164dd0
Sundew <guptaihita3@gmail.com>
2026-06-11 21:26:14 -0700

    added sample code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+import kglab
+
+# Create a KnowledgeGraph object
+kg = kglab.KnowledgeGraph()
+
+# Load RDF from a URL
+kg.load_rdf("https://storage.googleapis.com/kglab-tutorial/foaf.rdf", format="xml")
+
+# Measure the graph
+measure = kglab.Measure()
+measure.measure_graph(kg)
+
+print("Edges:", measure.get_edge_count())
+print("Nodes:", measure.get_node_count())
+
+# Serialize as Turtle/TTL
+ttl = kg.save_rdf_text()
+print(ttl)
